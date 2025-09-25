9649dce6b0cb4741476642855d71d02b64d7aacc
shaqlainmohammad <77788611+shaqlainmohammad@users.noreply.github.com>
2025-09-25 22:21:29 +0530

    Initial commit

---
+++ aws-cicd-demo/Controllers/HelloController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace YourNamespace.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public HelloController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Example: Read from appsettings.json
+            var message = _config.GetValue<string>("MyAppSettings:WelcomeMessage");
+            return Ok(new { Message = message });
+        }
+
+        [HttpPost]
+        public IActionResult Echo([FromBody] string value)
+        {
+            return Ok(new { YouSent = value });
+        }
+    }
+}
